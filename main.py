07a1847a9d39627a6a18c4fb851b386843fc0ab8
Lars <lars.eriksson.mail@gmail.com>
2022-03-21 21:33:17 +0100

    Finished the first webscraping script.

--- main.py
+++ main.py
@@ -6,8 +6,24 @@ site = requests.get("https://news.ycombinator.com/news").text
 
 soup = BeautifulSoup(site, "html.parser")
 
-article_text = soup.find(name='a', class_="titlelink").text
-article_link = soup.find(name='a').get("href")
-article_score = int(soup.find(name='span', class_="score").text.strip(" points"))
+article_t = []
+article_l = []
+article_s = []
 
-print(article_score)
+article = soup.find_all(name='a', class_="titlelink")
+for article_tag in article:
+    article_t.append(article_tag.getText())
+    article_l.append(soup.find(name='a').get("href"))
+
+article_score = soup.find_all(name='span', class_="score")
+for s in article_score:
+    article_s.append(int(s.getText().strip(" points")))
+
+max_index = 0
+highscore = 0
+
+for i in range(len(article_s)):
+    if article_s[i] >= highscore:
+        highscore = article_s[i]
+        max_index = i
+print(f"{article_t[max_index]} at {article_l[max_index]} with {article_s[max_index]} points is the winner")
